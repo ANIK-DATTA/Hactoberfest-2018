83d2cc19533b0d21b84c4eede18482b96559ef9f
Anik Datta <anikdatta05@gmail.com>
2019-10-31 13:12:50 +0530

    sparse to matrix format

---
+++ sparseM.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+void main()
+{  int a[20][20],s[20][3],m,n,i,j,k,b[20][20];
+printf("Enter row and column\n");
+scanf("%d%d",&m,&n);
+printf("Enter elements\n");
+for(i=0;i<m;i++){
+    for(j=0;j<n;j++){
+        scanf("%d",&a[i][j]);
+    }
+}
+printf("matrix is\n");
+for(i=0;i<m;i++){
+    for(j=0;j<n;j++){
+        printf("%5d",a[i][j]);
+    }  printf("\n");
+}
+s[0][0]=m;
+s[0][1]=n;
+k=1;
+for(i=0;i<m;i++){
+    for(j=0;j<n;j++){
+        if(a[i][j]!=0){
+            s[k][0]=i;
+            s[k][1]=j;
+            s[k][2]=a[i][j];
+            k++;
+        }
+    }
+}  s[0][2]=k-1;
+printf("\nEfficient representation \n");
+for(i=0;i<k;i++){
+    for(j=0;j<3;j++){
+        printf("%5d",s[i][j]);
+    }  printf("\n");
+}
+k=1;
+for(i=0;i<m;i++){
+    for(j=0;j<n;j++){
+        if(i==s[k][0]&&j==s[k][1]){
+            b[i][j]=s[k][2];
+            k++;
+        }
+        else {
+            b[i][j]=0;
+        }
+    }
+}
+printf("\nMatrix\n");
+for(i=0;i<m;i++){
+    for(j=0;j<n;j++){
+        printf("%5d",b[i][j]);
+    }  printf("\n");
+}
+}
